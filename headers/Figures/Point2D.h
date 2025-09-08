2bc44e83091f71a284bf68f5247a752a5ab5e1a3
ardrass <Ardrass@mail.ru>
2025-09-09 01:04:10 +0500

    moved from namespace info to point2d info

--- headers/Figures/Point2D.h
+++ headers/Figures/Point2D.h
@@ -1,13 +1,14 @@
 #ifndef OURPAINTDCM_HEADERS_FIGURES_POINT2D_H
 #define OURPAINTDCM_HEADERS_FIGURES_POINT2D_H
-/**
-* @brief 2D point structure.
-*
-* Represents a point in 2D space with X and Y
-* Basic object for circles, arcs, sections etc.
-*/
+
 namespace OurPaintDCM {
     namespace Figures {
+        /**
+        * @brief 2D point structure.
+        *
+        * Represents a point in 2D space with X and Y
+        * Basic object for circles, arcs, sections etc.
+        */
         struct Point2D {
             double _x; ///< X coordinates
             double _y; ///< Y coordinates
