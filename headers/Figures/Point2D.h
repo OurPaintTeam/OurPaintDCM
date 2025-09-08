73356b3f2b8e4785c7062bd3d721fa817b48c481
ardrass <Ardrass@mail.ru>
2025-09-09 00:30:14 +0500

    fixed ; after struct

--- headers/Figures/Point2D.h
+++ headers/Figures/Point2D.h
@@ -4,14 +4,14 @@
 * @brief 2D point structure.
 *
 * Represents a point in 2D space with X and Y
-* Basic object for circles, arcs, sections and etc.
+* Basic object for circles, arcs, sections etc.
 */
 namespace OurPaintDCM {
     namespace Figures {
         struct Point2D {
             double _x; ///< X coordinates
             double _y; ///< Y coordinates
-        }
+        };
     }
 }
 #endif //OURPAINTDCM_HEADERS_FIGURES_POINT2D_H
