3da2d27a3c4acf7f39f2a457022a6cdf5d13282f
ardrass <Ardrass@mail.ru>
2025-09-09 00:16:51 +0500

    created point struct

---
+++ headers/Figures/Point2D.h
@@ -0,0 +1,17 @@
+#ifndef POINT_H
+#define POINT_H
+/**
+* @brief 2D point structure.
+*
+* Represents a point in 2D space with X and Y
+* Basic object for circles, arcs, sections and etc.
+*/
+namespace OurPaintDCM {
+    namespace Figures {
+        struct Point2D {
+            double _x; ///< X coordinates
+            double _y; ///< Y coordinates
+        }
+    }
+}
+#endif //POINT_H
