e33e470899eefb011e1a784345b1bd292db94da8
ardrass <Ardrass@mail.ru>
2025-09-09 17:08:34 +0500

    fixed test to new hierarchy

--- tests/Figures/Point2DGTEST.cpp
+++ tests/Figures/Point2DGTEST.cpp
@@ -9,29 +9,29 @@ protected:
     Point2D p;
 
     void SetUp() override {
-        p._x = 0.0;
-        p._y = 0.0;
+        p.x() = 0.0;
+        p.y() = 0.0;
     }
 };
 
 TEST_F(Point2DTest, DefaultCoordinates) {
-    EXPECT_DOUBLE_EQ(p._x, 0.0);
-    EXPECT_DOUBLE_EQ(p._y, 0.0);
+    EXPECT_DOUBLE_EQ(p.x(), 0.0);
+    EXPECT_DOUBLE_EQ(p.y(), 0.0);
 }
 
 TEST_F(Point2DTest, SetCoordinates) {
-    p._x = 5.5;
-    p._y = -3.2;
-    EXPECT_DOUBLE_EQ(p._x, 5.5);
-    EXPECT_DOUBLE_EQ(p._y, -3.2);
+    p.x() = 5.5;
+    p.y() = -3.2;
+    EXPECT_DOUBLE_EQ(p.x(), 5.5);
+    EXPECT_DOUBLE_EQ(p.y(), -3.2);
 }
 TEST_F(Point2DTest, DistanceToAnotherPoint) {
     Point2D p2;
-    p2._x = 3.0;
-    p2._y = 4.0;
+    p2.x() = 3.0;
+    p2.y() = 4.0;
 
-    double dx = p2._x - p._x;
-    double dy = p2._y - p._y;
+    double dx = p2.x() - p.x();
+    double dy = p2.y() - p.y();
     double distance = std::sqrt(dx * dx + dy * dy);
 
     EXPECT_DOUBLE_EQ(distance, 5.0);
