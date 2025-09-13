eab2314cefc6330c91f45b67479d2a81631b9496
ardrass <Ardrass@mail.ru>
2025-09-13 14:20:09 +0500

    added second requirement

--- headers/Requirements/Requirements.h
+++ headers/Requirements/Requirements.h
@@ -14,86 +14,108 @@ namespace OurPaintDCM::Requirements {
 * into a mathematical function (equation) that must be satisfied
 * by the solver.
 */
-class Requirement {
-protected:
-    RequirementType type; ///< Type of the requirement (distance, angle, coincidence, etc.)
-public:
-    /**
-    * @brief Construct a new Requirement.
-    * @param t The type of this requirement.
-    */
-    explicit Requirement(RequirementType t)
-        : type(t) {
-    }
+    class Requirement {
+    protected:
+        RequirementType type; ///< Type of the requirement (distance, angle, coincidence, etc.)
+    public:
+        /**
+        * @brief Construct a new Requirement.
+        * @param t The type of this requirement.
+        */
+        explicit Requirement(RequirementType t)
+            : type(t) {
+        }
 
-    /// Virtual destructor.
-    virtual ~Requirement() = default;
-    /**
-     * @brief Get the type of this requirement.
-     * @return The requirement type.
-     */
-    RequirementType getType() const noexcept { return type; }
-    /**
-     * @brief Convert this requirement into a mathematical function.
-     *
-     * Each requirement generates one function
-     * (equations) that the global solver must satisfy.
-     *
-     * @return Pointer to a Function object representing this requirement.
-     */
-    virtual ErrorFunction* toFunction() = 0;
-};
+        /// Virtual destructor.
+        virtual ~Requirement() = default;
+        /**
+         * @brief Get the type of this requirement.
+         * @return The requirement type.
+         */
+        RequirementType getType() const noexcept { return type; }
+        /**
+         * @brief Convert this requirement into a mathematical function.
+         *
+         * Each requirement generates one function
+         * (equations) that the global solver must satisfy.
+         *
+         * @return Pointer to a Function object representing this requirement.
+         */
+        virtual ErrorFunction* toFunction() = 0;
+    };
 
-/**
- * @brief Requirement: fixed distance between point and sections.
- *
- * This requirement enforces that the Euclidean distance
- * between point and section is equal to the specified value.
- */
-class PointLineDist final : public Requirement {
-    Figures::Point2D*                             _p;    ///< pointer to point.
-    Figures::Line<Figures::Point2D>* _l;    ///< pointer to line.
-    double                                                     _dist; ///< distance between point and line
-public:
     /**
-     * @brief Construct a new PointSectionDist requirement.
-     * @param p First point.
-     * @param l Second point.
-     * @param dist Fixed distance value between the point and line.
+     * @brief Requirement: fixed distance between point and sections.
+     *
+     * This requirement enforces that the Euclidean distance
+     * between point and section is equal to the specified value.
      */
-    PointLineDist(Figures::Point2D*                             p,
-                     Figures::Line<Figures::Point2D>* l,
-                     double                                                     dist);
+    class PointLineDist final: public Requirement {
+        Figures::Point2D*                             _p;    ///< pointer to point.
+        Figures::Line<Figures::Point2D>*              _l;    ///< pointer to line.
+        double                                        _dist; ///< distance between point and line
+    public:
+        /**
+         * @brief Construct a new PointSectionDist requirement.
+         * @param p Point.
+         * @param l Line.
+         * @param dist Fixed distance value between the point and line.
+         */
+        PointLineDist(Figures::Point2D*                             p,
+                         Figures::Line<Figures::Point2D>* l,
+                         double                                                     dist);
+        /**
+          * @brief Build the function for the point section distance constraint.
+          *
+          * The equation is derived from the general line equation through
+          * segment endpoints A(xₐ, yₐ), B(xᵦ, yᵦ):
+          *
+          * \f[
+          * \begin{aligned}
+          * Ax + By + C &= 0 \\
+          * A &= y_B - y_A \\
+          * B &= x_A - x_B \\
+          * C &= x_B y_A - x_A y_B
+          * \end{aligned}
+          * \f]
+          *
+          * For a point P(x, y), the signed distance to line AB is:
+          *
+          * \f[
+          * F = \frac{A x_P + B y_P + C}{\sqrt{A^2 + B^2}}
+          * \f]
+          *
+          * The requirement enforces:
+          *
+          * \f[ F - d = 0 \f]
+          *
+          * where \f$ d \f$ is the fixed distance value.
+          *
+          * @return Pointer to function object tree.
+          */
+        ErrorFunction* toFunction() override;
+    };
     /**
-  * @brief Build the function for the point section distance constraint.
-  *
-  * The equation is derived from the general line equation through
-  * segment endpoints A(xₐ, yₐ), B(xᵦ, yᵦ):
-  *
-  * \f[
-  * \begin{aligned}
-  * Ax + By + C &= 0 \\
-  * A &= y_B - y_A \\
-  * B &= x_A - x_B \\
-  * C &= x_B y_A - x_A y_B
-  * \end{aligned}
-  * \f]
-  *
-  * For a point P(x, y), the signed distance to line AB is:
-  *
-  * \f[
-  * F = \frac{A x_P + B y_P + C}{\sqrt{A^2 + B^2}}
-  * \f]
-  *
-  * The requirement enforces:
-  *
-  * \f[ F - d = 0 \f]
-  *
-  * where \f$ d \f$ is the fixed distance value.
-  *
-  * @return Pointer to function object tree.
-  */
-    ErrorFunction* toFunction() override;
-};
+     * @brief Requirement: put the point to the section.
+     *
+     * This requirement enforces that the Euclidean distance
+     * between point and section is equal to 0.
+     */
+    class PointOnLine final : public Requirement{
+        Figures::Point2D*                             _p;    ///< pointer to point.
+        Figures::Line<Figures::Point2D>*              _l;    ///< pointer to line.
+        public:
+        /**
+         * @brief Construct a new PointOnLine requirement.
+         * @param p Point.
+         * @param l Line.
+         */
+        PointOnLine (Figures::Point2D* p, Figures::Line<Figures::Point2D>* l);
+        /**
+         * @brief Simular to PointLineDist.
+         * @return Pointer to function object tree.
+         */
+        ErrorFunction* toFunction() override;
+    };
 }
 #endif //OURPAINTDCM_HEADERS_REQUIREMENTS_REQUIREMENTS_H
--- src/Requirements/Requirements.cpp
+++ src/Requirements/Requirements.cpp
@@ -1,17 +1,22 @@
 #include "Requirements.h"
 
-namespace OurPaintDCM::Requirements{
+namespace OurPaintDCM::Requirements {
+// ----------------------------------------------------------------
     PointLineDist::PointLineDist(Figures::Point2D* p, Figures::Line<Figures::Point2D>* l, double dist)
-        : Requirement(RequirementType::ET_POINTLINEDIST), _p(p), _l(l), _dist(dist)
-    {}
+        : Requirement(RequirementType::ET_POINTLINEDIST),
+          _p(p),
+          _l(l),
+          _dist(dist) {
+    }
+
     ErrorFunction* PointLineDist::toFunction() {
-        std::vector<Variable *> x;
-        auto* p_x = new Variable(_p->ptrX());
-        auto* p_y = new Variable(_p->ptrY());
-        auto* l_startx = new Variable(_l->p1->ptrX());
-        auto* l_starty = new Variable(_l->p1->ptrY());
-        auto* l_endx = new Variable(_l->p2->ptrX());
-        auto* l_endy = new Variable(_l->p2->ptrY());
+        std::vector<Variable*> x;
+        auto*                  p_x      = new Variable(_p->ptrX());
+        auto*                  p_y      = new Variable(_p->ptrY());
+        auto*                  l_startx = new Variable(_l->p1->ptrX());
+        auto*                  l_starty = new Variable(_l->p1->ptrY());
+        auto*                  l_endx   = new Variable(_l->p2->ptrX());
+        auto*                  l_endy   = new Variable(_l->p2->ptrY());
         x.push_back(p_x);
         x.push_back(p_y);
         x.push_back(l_startx);
@@ -20,4 +25,27 @@ namespace OurPaintDCM::Requirements{
         x.push_back(l_endy);
         return new PointSectionDistanceError(x, _dist);
     }
+
+    //-----------------------------------------------------------------
+    PointOnLine::PointOnLine(Figures::Point2D* p, Figures::Line<Figures::Point2D>* l)
+        : Requirement(RequirementType::ET_POINTONLINE),
+          _p(p),
+          _l(l){}
+    ErrorFunction* PointOnLine::toFunction() {
+        std::vector<Variable*> x;
+        auto*                  p_x      = new Variable(_p->ptrX());
+        auto*                  p_y      = new Variable(_p->ptrY());
+        auto*                  l_startx = new Variable(_l->p1->ptrX());
+        auto*                  l_starty = new Variable(_l->p1->ptrY());
+        auto*                  l_endx   = new Variable(_l->p2->ptrX());
+        auto*                  l_endy   = new Variable(_l->p2->ptrY());
+        x.push_back(p_x);
+        x.push_back(p_y);
+        x.push_back(l_startx);
+        x.push_back(l_starty);
+        x.push_back(l_endx);
+        x.push_back(l_endy);
+        return new PointOnSectionError(x);
+    }
+
 }
