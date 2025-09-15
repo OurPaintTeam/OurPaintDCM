0b795b4ea829527f57bf79a7f7d0b7d637c83b91
ardrass <Ardrass@mail.ru>
2025-09-15 19:55:16 +0500

    realize all requirements

--- src/Requirements/Requirements.cpp
+++ src/Requirements/Requirements.cpp
@@ -109,7 +109,7 @@ ErrorFunction* LineCircleDist::toFunction() {
     auto*                  l_endy   = new Variable(_l->p2->ptrY());
     auto*                  centerx  = new Variable(_c->center->ptrX());
     auto*                  centery  = new Variable(_c->center->ptrY());
-    auto*                  radius = new Variable(&_c->radius);
+    auto*                  radius   = new Variable(&_c->radius);
     x.push_back(l_startx);
     x.push_back(l_starty);
     x.push_back(l_endx);
@@ -119,6 +119,174 @@ ErrorFunction* LineCircleDist::toFunction() {
     x.push_back(radius);
     return new SectionCircleDistanceError(x, _dist);
 }
-// ------------------------------------------------------------------
 
+// ------------------------------------------------------------------
+LineOnCircle::LineOnCircle(Figures::Line<Figures::Point2D>* line, Figures::Circle<Figures::Point2D>* circle)
+    : Requirement(RequirementType::ET_LINEONCIRCLE),
+      _l(line),
+      _c(circle) {
+}
+
+ErrorFunction* LineOnCircle::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l_startx = new Variable(_l->p1->ptrX());
+    auto*                  l_starty = new Variable(_l->p1->ptrY());
+    auto*                  l_endx   = new Variable(_l->p2->ptrX());
+    auto*                  l_endy   = new Variable(_l->p2->ptrY());
+    auto*                  centerx  = new Variable(_c->center->ptrX());
+    auto*                  centery  = new Variable(_c->center->ptrY());
+    auto*                  radius   = new Variable(&_c->radius);
+    x.push_back(l_startx);
+    x.push_back(l_starty);
+    x.push_back(l_endx);
+    x.push_back(l_endy);
+    x.push_back(centerx);
+    x.push_back(centery);
+    x.push_back(radius);
+    return new SectionOnCircleError(x);
+}
+
+// ----------------------------------------------------------------
+LineInCircle::LineInCircle(Figures::Line<Figures::Point2D>* line, Figures::Circle<Figures::Point2D>* circle)
+    : Requirement(RequirementType::ET_LINEINCIRCLE),
+      _l(line),
+      _c(circle) {
+}
+
+ErrorFunction* LineInCircle::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l_startx = new Variable(_l->p1->ptrX());
+    auto*                  l_starty = new Variable(_l->p1->ptrY());
+    auto*                  l_endx   = new Variable(_l->p2->ptrX());
+    auto*                  l_endy   = new Variable(_l->p2->ptrY());
+    auto*                  centerx  = new Variable(_c->center->ptrX());
+    auto*                  centery  = new Variable(_c->center->ptrY());
+    auto*                  radius   = new Variable(&_c->radius);
+    x.push_back(l_startx);
+    x.push_back(l_starty);
+    x.push_back(l_endx);
+    x.push_back(l_endy);
+    x.push_back(centerx);
+    x.push_back(centery);
+    x.push_back(radius);
+    return new SectionInCircleError(x);
+}
+
+// -----------------------------------------------------------------
+
+LineLineParallel::LineLineParallel(Figures::Line<Figures::Point2D>* l1, Figures::Line<Figures::Point2D>* l2)
+    : Requirement(RequirementType::ET_LINELINEPARALLEL),
+      _l1(l1),
+      _l2(l2) {
+}
+
+ErrorFunction* LineLineParallel::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l1_startx = new Variable(_l1->p1->ptrX());
+    auto*                  l1_starty = new Variable(_l1->p1->ptrY());
+    auto*                  l1_endx   = new Variable(_l1->p2->ptrX());
+    auto*                  l1_endy   = new Variable(_l1->p2->ptrY());
+    auto*                  l2_startx = new Variable(_l2->p1->ptrX());
+    auto*                  l2_starty = new Variable(_l2->p1->ptrY());
+    auto*                  l2_endx   = new Variable(_l2->p2->ptrX());
+    auto*                  l2_endy   = new Variable(_l2->p2->ptrY());
+    x.push_back(l1_startx);
+    x.push_back(l1_starty);
+    x.push_back(l1_endx);
+    x.push_back(l1_endy);
+    x.push_back(l2_startx);
+    x.push_back(l2_starty);
+    x.push_back(l2_endx);
+    x.push_back(l2_endy);
+    return new SectionSectionParallelError(x);
+}
+
+// -------------------------------------------------------------------
+LineLinePerpendicular::LineLinePerpendicular(Figures::Line<Figures::Point2D>* l1, Figures::Line<Figures::Point2D>* l2)
+    : Requirement(RequirementType::ET_LINELINEPERPENDICULAR),
+      _l1(l1),
+      _l2(l2) {
+}
+
+ErrorFunction* LineLinePerpendicular::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l1_startx = new Variable(_l1->p1->ptrX());
+    auto*                  l1_starty = new Variable(_l1->p1->ptrY());
+    auto*                  l1_endx   = new Variable(_l1->p2->ptrX());
+    auto*                  l1_endy   = new Variable(_l1->p2->ptrY());
+    auto*                  l2_startx = new Variable(_l2->p1->ptrX());
+    auto*                  l2_starty = new Variable(_l2->p1->ptrY());
+    auto*                  l2_endx   = new Variable(_l2->p2->ptrX());
+    auto*                  l2_endy   = new Variable(_l2->p2->ptrY());
+    x.push_back(l1_startx);
+    x.push_back(l1_starty);
+    x.push_back(l1_endx);
+    x.push_back(l1_endy);
+    x.push_back(l2_startx);
+    x.push_back(l2_starty);
+    x.push_back(l2_endx);
+    x.push_back(l2_endy);
+    return new SectionSectionPerpendicularError(x);
+}
+
+// -----------------------------------------------------------------------
+LineLineAngle::LineLineAngle(Figures::Line<Figures::Point2D>* l1, Figures::Line<Figures::Point2D>* l2, double angle)
+    : Requirement(RequirementType::ET_LINELINEANGLE),
+      _l1(l1),
+      _l2(l2),
+      _angle(angle) {
+}
+
+ErrorFunction* LineLineAngle::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l1_startx = new Variable(_l1->p1->ptrX());
+    auto*                  l1_starty = new Variable(_l1->p1->ptrY());
+    auto*                  l1_endx   = new Variable(_l1->p2->ptrX());
+    auto*                  l1_endy   = new Variable(_l1->p2->ptrY());
+    auto*                  l2_startx = new Variable(_l2->p1->ptrX());
+    auto*                  l2_starty = new Variable(_l2->p1->ptrY());
+    auto*                  l2_endx   = new Variable(_l2->p2->ptrX());
+    auto*                  l2_endy   = new Variable(_l2->p2->ptrY());
+    x.push_back(l1_startx);
+    x.push_back(l1_starty);
+    x.push_back(l1_endx);
+    x.push_back(l1_endy);
+    x.push_back(l2_startx);
+    x.push_back(l2_starty);
+    x.push_back(l2_endx);
+    x.push_back(l2_endy);
+    return new SectionSectionAngleError(x, _angle);
+}
+
+// ---------------------------------------------------------------------
+LineHorizontal::LineHorizontal(Figures::Line<Figures::Point2D>* l) : Requirement(RequirementType::ET_HORIZONTAL),
+_l(l){}
+ErrorFunction* LineHorizontal::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l1_startx = new Variable(_l->p1->ptrX());
+    auto*                  l1_starty = new Variable(_l->p1->ptrY());
+    auto*                  l1_endx   = new Variable(_l->p2->ptrX());
+    auto*                  l1_endy   = new Variable(_l->p2->ptrY());
+    x.push_back(l1_startx);
+    x.push_back(l1_starty);
+    x.push_back(l1_endx);
+    x.push_back(l1_endy);
+    return new HorizontalError(x);
+}
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+LineVertical::LineVertical(Figures::Line<Figures::Point2D>* l) : Requirement(RequirementType::ET_VERTICAL),
+_l(l){}
+ErrorFunction* LineVertical::toFunction() {
+    std::vector<Variable*> x;
+    auto*                  l1_startx = new Variable(_l->p1->ptrX());
+    auto*                  l1_starty = new Variable(_l->p1->ptrY());
+    auto*                  l1_endx   = new Variable(_l->p2->ptrX());
+    auto*                  l1_endy   = new Variable(_l->p2->ptrY());
+    x.push_back(l1_startx);
+    x.push_back(l1_starty);
+    x.push_back(l1_endx);
+    x.push_back(l1_endy);
+    return new VerticalError(x);
+}
 }
