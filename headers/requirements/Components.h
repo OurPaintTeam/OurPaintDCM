c89e6589703270639e379139ada54ba80164df41
ardrass <Ardrass@mail.ru>
2025-10-04 02:08:47 +0500

    added components

---
+++ headers/requirements/Components.h
@@ -0,0 +1,69 @@
+#ifndef HEADERS_REQUIREMENTS_COMPONENTS_H
+#define HEADERS_REQUIREMENTS_COMPONENTS_H
+#include <vector>
+#include <set>
+//forward declaration
+class ErrorFunction;
+class Variable;
+namespace OurPaintDCM::Utils {
+struct ID;
+}
+
+
+
+namespace OurPaintDCM::Requirements {
+/**
+ * @brief struct which describe one item of component
+ */
+struct ComponentElement {
+    Utils::ID id; ///< ID of requirement(in graph it is edge)
+    ErrorFunction* func; ///< Function of Error that used in solving
+    ~ComponentElement() {
+        delete func;
+    }
+};
+
+/**
+ * @brief class which describe all components
+ *
+ * This class is used to store all components
+ * and to delete them
+ *
+ */
+class Components {
+    std::vector<ComponentElement> _elems; ///< vector of components
+    std::set<Variable*> _vars; ///< set of variables
+    public:
+    /**
+     * @brief default constructor
+     */
+    Components(): _elems(), _vars() {}
+
+    /**
+     * @brief add element to components
+     * @param element - element to add
+     */
+    void addElement(ComponentElement& element) {
+        _elems.push_back(element);
+        for (auto& var : element.func->getVariables()) {
+            _vars.insert(var);
+        }
+    }
+    /**
+     *@brief delete element from components
+     * @param id - ID of element
+     */
+    void deleteElement(const Utils::ID& id) {
+        for (auto it = _elems.begin(); it != _elems.end(); ++it) {
+            if (it->id == id) {
+                for (auto& var : it->func->getVariables()) {
+                    _vars.erase(var);
+                }
+                _elems.erase(it);
+                break;
+            }
+        }
+    }
+};
+}
+#endif //HEADERS_REQUIREMENTS_COMPONENTS_H
